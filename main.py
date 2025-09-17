d59391a29b7cbf4282e38c53fdfe9fd7f431c1d1
Fakai Zhao <fakzhao@cisco.com>
2025-09-17 15:27:00 +0800

    first commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ nonce = str(random.randint(10000000, 99999999))
 
 
 def test_get():
-    sign_str = client_id + timestamp + nonce
+    sign_str = client_id + business_id + timestamp + nonce
     signature = hashlib.sha256(sign_str.encode('utf-8')).hexdigest()[:32]
     headers = {
         "X-Client-Id": client_id,
@@ -43,7 +43,7 @@ def test_post():
     }
     encrypted_data = encrypt_sm2(json.dumps(data))
     print("base64 decoded encrypted data:", base64.b64decode(encrypted_data))
-    sign_str = encrypted_data + client_id + timestamp + nonce
+    sign_str = encrypted_data + client_id + business_id + timestamp + nonce
     print(sign_str)
     signature = hashlib.sha256(sign_str.encode('utf-8')).hexdigest()[:32]
     headers = {
